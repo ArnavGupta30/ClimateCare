10721bb1ab2668fc8e47d786a205927badf0e5f6
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2023-07-21 02:18:43 +0530

    port issue

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.use("*", (req, res) => {
 
 // listen
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000 ... http://localhost:3000');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on port 3000');
 })
