540dda115d699b3cfaedfbe4608d6bc9c1dcdac7
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2023-07-21 02:32:42 +0530

    minor bug fix

--- index.js
+++ index.js
@@ -40,5 +40,5 @@ app.use("*", (req, res) => {
 // listen
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log('Server is running on port 3000');
+  console.log('Server is running on port ' + (process.env.PORT || 3000));
 })
